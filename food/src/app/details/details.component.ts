2c343e2054c65abebb0d13f0df04d1b1f49d7ced
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-12-20 19:11:08 +0000

    5.0 - 6.0 - 7.0

--- food/src/app/details/details.component.ts
+++ food/src/app/details/details.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import {Location} from '@angular/common'
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent {
+  routeObs: Observable<ParamMap>;
 
-  constructor() { }
+  prod : any; //Qui salverò la traccia selezionata
+  foodServiceObs: Observable<Object>;
+
+  //Usiamo la dependency injection per farci mandare i moduli del routing e dello
+  //SpotifyService
+  constructor(
+    private route: ActivatedRoute,
+    private service: FoodService,
+    private location: Location ) { }
+
 
   ngOnInit(): void {
+    //Ottengo l'observable che notifica le informazioni sulla route attiva
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
+  }
+
+  //Ogni volta che viene invocata la route tracks/:id, l'observable richiama questo metodo
+  getRouterParam = (params: ParamMap) =>
+  {
+    let productId = params.get('id'); //Ottengo l'id dai parametri
+    console.log (productId); //Stampo su console
+    //spotifyServiceObs va dichiarato
+    this.foodServiceObs = this.service.foodDetails(productId) ;
+    this.foodServiceObs.subscribe((data)=>this.prod = data)
+  }
+
+
+  back() : void
+  {
+    this.location.back();
   }
 
 }
--- food/src/app/food.service.ts
+++ food/src/app/food.service.ts
@@ -8,7 +8,12 @@ export class FoodService {
 
   constructor(private http: HttpClient) { }
   searchFood(query: string) {
-    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=2&json=true`;
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=5&json=true`;
+    const headers = new HttpHeaders("Content-Type");
+    return this.http.get(url, { headers });
+  }
+  foodDetails(id: string) {
+    const url = `https://world.openfoodfacts.org/api/v0/product/${id}`;
     const headers = new HttpHeaders("Content-Type");
     return this.http.get(url, { headers });
   }
