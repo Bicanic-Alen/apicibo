79703019f4535783235df6edb8363400d1945ce9
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-12-20 17:49:46 +0000

    4.0

--- food/src/app/search/search.component.ts
+++ food/src/app/search/search.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../food.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  query: string;
+  title = 'food';
+  obsFood: Observable<Object>;
+  results: any;
+  // faccio iniettare lo spotify service e faccio una ricerca
+  constructor(public food: FoodService) {
 
-  constructor() { }
+  }
+  submit(query: HTMLInputElement): void {
 
-  ngOnInit(): void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsFood = this.food.searchFood(this.query);
+    this.obsFood.subscribe((data) => { this.results = data; console.log(this.results) });
   }
 
 }
+
